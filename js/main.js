0b94074da4fe94cb051d5494f1b3fc3c0a339f45
rhuanbello <rhuanbello@gmail.com>
2021-07-14 16:46:36 -0300

    Cultflix 2.0.0

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:20,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:8
+        }
+    }
+})
